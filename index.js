0512b07d22f0d9816d3c90b43166ae2a21dd24ab
HuyNT <huynt.me@gmail.com>
2020-12-07 16:59:52 +0700

    first Commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(express.static("./public"));
+
+mongoose.connect(process.env.CONNECT_DATA, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+mongoose.connection.on("connected",
+app.listen(8080, () => {
+    console.log("Server is running");
+  })
+)
