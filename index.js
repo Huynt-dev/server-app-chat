eb4e306bf5d08a2f57f1469b9a580b273d6c59c2
HuyNT <huynt.me@gmail.com>
2020-12-21 23:15:25 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
 const app = express();
+const jwt = require("jsonwebtoken");
 
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
@@ -28,9 +29,25 @@ mongoose.connect(process.env.CONNECT_DATA, {
 
 io.on("connection", (client) => {
   console.log("ket noi " + client.id);
-  client.on("send-data", function (data) {
+
+  client.on("data-chat", function (data) {
     console.log(client.id + "chat:" + data);
+    // luu vao db user nao => mesage nao => room nao
+    // messages: user, message, room
   });
+});
+
+io.use(async (socket, next) => {
+  if (socket.handshake.headers && socket.handshake.headers.authorization) {
+    const token = await jwt.verify(
+      socket.handshake.headers.authorization,
+      process.env.JWT_KEY
+    );
+
+    console.log(token);
+  } else {
+    next(new Error("Authentication error"));
+  }
 });
 
 app.get("/", function (req, res) {
