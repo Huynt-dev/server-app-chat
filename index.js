f3874a054f8d15769f0e31f43aa733d41fb12c91
HuyNT <huynt.me@gmail.com>
2020-12-24 17:26:06 +0700

    ok

--- index.js
+++ index.js
@@ -31,10 +31,8 @@ io.on("connection", (client) => {
   console.log("ket noi " + client.id);
 
   client.on("data-chat", function (data) {
-    console.log(client.id + "chat:" + data);
+    // console.log(client.id + "chat:" + data);
     io.sockets.emit("data-chat-user", data);
-    // luu vao db user nao => mesage nao => room nao
-    // messages: user, message, room
   });
 });
 
@@ -50,6 +48,19 @@ io.on("connection", (client) => {
 //     next(new Error("Authentication error"));
 //   }
 // });
+
+// io.sockets
+//   .on(
+//     "connection",
+//     socketioJwt.authorize({
+//       secret: process.env.JWT_KEY,
+//       timeout: 15000, // 15 seconds to send the authentication message
+//     })
+//   )
+//   .on("authenticated", (socket) => {
+//     //this socket is authenticated, we are good to handle more events from it.
+//     console.log(`hello! ${socket.decoded_token.name}`);
+//   });
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
